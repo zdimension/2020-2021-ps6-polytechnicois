1cca1c010f8e5af3b5f01731ea95ccb52fa4d8c2
Emmeline Vouriot <emmeline.vouriot@etu.univ-cotedazur.fr>
2021-04-01 10:46:55 +0200

    add style to connecte

--- frontend/src/app/connecte/connecte.component.ts
+++ frontend/src/app/connecte/connecte.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: "app-connecte",
-    templateUrl: "./connecte.component.html"/*,
-    styleUrls: ["./user.component.scss"]*/
+    templateUrl: "./connecte.component.html",
+    styleUrls: ["./connecte.component.scss"]
 })
 export class ConnecteComponent implements OnInit
 {
