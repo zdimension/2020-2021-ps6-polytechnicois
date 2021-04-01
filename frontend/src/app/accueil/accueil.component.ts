b457c36ab44cd9c0764d9ec63882b5ba952c2171
Emmeline Vouriot <emmeline.vouriot@etu.univ-cotedazur.fr>
2021-04-01 10:55:02 +0200

    fix accueil style

--- frontend/src/app/accueil/accueil.component.ts
+++ frontend/src/app/accueil/accueil.component.ts
@@ -6,8 +6,8 @@ import { Router } from "@angular/router";
 
 @Component({
     selector: "app-accueil",
-    templateUrl: "./accueil.component.html"/*,
-    styleUrls: ["./accueil.component.scss"]*/
+    templateUrl: "./accueil.component.html",
+    styleUrls: ["./accueil.component.scss"]
 })
 export class AccueilComponent implements OnInit
 {
