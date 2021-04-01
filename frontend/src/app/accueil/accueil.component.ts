e7afd69f1b34c8129df7bdd7e9ce558237c967bc
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2021-04-01 10:34:08 +0200

    Implement connection mechanism, not terminated #7

--- frontend/src/app/accueil/accueil.component.ts
+++ frontend/src/app/accueil/accueil.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
+import { UserService } from "../../services/user.service";
 
 @Component({
     selector: "app-accueil",
-    templateUrl: "./accueil.component.html",
-    styleUrls: ["./accueil.component.scss"]
+    templateUrl: "./accueil.component.html"/*,
+    styleUrls: ["./accueil.component.scss"]*/
 })
 export class AccueilComponent implements OnInit
 {
 
     public loginForm: FormGroup;
 
-    constructor(public formBuilder: FormBuilder)
+    constructor(public formBuilder: FormBuilder, public userService: UserService)
     {
         this.loginForm = this.formBuilder.group({
             login: [""],
@@ -26,5 +27,8 @@ export class AccueilComponent implements OnInit
 
     connexion(): void {
         console.log("Try to connect");
+        let login=this.loginForm.get('login').value;
+        let pass=this.loginForm.get('pass').value;
+        this.userService.connexion(login, pass);
     }
 }
