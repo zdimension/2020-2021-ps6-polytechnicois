64a5b3ed47381aac72e5daad0e2f9db62a933c82
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2021-04-04 10:22:24 +0200

    creationquiz -> begin to write form #6

--- frontend/src/app/creationquiz/creationquiz.component.ts
+++ frontend/src/app/creationquiz/creationquiz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormGroup } from "@angular/forms";
 
 @Component({
     selector: "app-creationquiz",
@@ -7,6 +8,9 @@ import { Component, OnInit } from "@angular/core";
 })
 export class CreationQuizComponent implements OnInit
 {
+
+    public creationQuizFirstStepForm: FormGroup;
+    public listdifficulte: number[];
 
     constructor()
     {
