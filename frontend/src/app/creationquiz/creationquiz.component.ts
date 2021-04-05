3792f5bf814f9c7ca56734ed5e422a6c0b9b5d6d
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2021-04-05 09:54:25 +0200

    creationquiz -> add new inputs #6

--- frontend/src/app/creationquiz/creationquiz.component.ts
+++ frontend/src/app/creationquiz/creationquiz.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup } from "@angular/forms";
+import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
+import { QuizTheme } from "../../models/quiztheme.model";
+import { QuizService } from "../../services/quiz.service";
 
 @Component({
     selector: "app-creationquiz",
@@ -10,13 +12,30 @@ export class CreationQuizComponent implements OnInit
 {
 
     public creationQuizFirstStepForm: FormGroup;
-    public listdifficulte: number[];
+    public listDifficulte: number[]=[1, 2, 3, 4, 5];
+    public listThemes: QuizTheme[]=[];
+    creerTheme: boolean=false;
 
-    constructor()
+    constructor(public formBuilder: FormBuilder, public quizService: QuizService)
     {
+        this.creationQuizFirstStepForm=this.formBuilder.group({
+            nomquiz: new FormControl(),
+            difficulte: new FormControl(0),
+            theme: new FormControl(0),
+            nomnouveautheme: new FormControl()
+        });
     }
 
     ngOnInit(): void
     {
+        this.quizService.themes$.subscribe((themes) =>
+        {
+            return this.listThemes = themes;
+        });
+    }
+
+    ajouterTheme(): void
+    {
+        //
     }
 }
