af38c373d50c839838e92d503be2304cf8b41cd4
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2021-04-11 16:33:18 +0200

    recap -> ok #11

--- frontend/src/app/recap/recap.component.ts
+++ frontend/src/app/recap/recap.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from "@angular/core";
 import { QuizService } from "../../services/quiz.service";
 import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
 import { UserService } from "../../services/user.service";
-import { Router } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Quiz } from "../../models/quiz.model";
+import { Question } from "../../models/question.model";
 
 @Component({
     selector: "app-recap",
@@ -12,11 +14,62 @@ import { Router } from "@angular/router";
 export class RecapComponent implements OnInit
 {
 
-    constructor(public quizService: QuizService, public formBuilder: FormBuilder, public userService: UserService, private router: Router)
+    private numQuestion: number=0;
+    private urlImage: string="";
+    private question: string="";
+    private goodAnswer: string="";
+    private quiz: Quiz=null;
+    private id: number=1;
+
+    constructor(public quizService: QuizService, public userService: UserService, private router: Router, private route: ActivatedRoute)
     {
     }
 
     ngOnInit(): void
     {
+        this.getQuiz();
+        this.numQuestion=0;
+    }
+
+    getQuiz(): void
+    {
+        this.id = +this.route.snapshot.paramMap.get("id");
+        this.quizService.getQuizById(this.id)
+            .subscribe(quiz =>
+            {
+                this.quiz=quiz;
+                this.showQuestion();
+            });
+    }
+
+    private showQuestion(): void {
+        if(this.quiz === null) {
+            return;
+        }
+        let question: Question = this.quiz.questions[this.numQuestion];
+        this.question=question.label;
+        this.urlImage=question.image;
+        if(this.urlImage === undefined) {
+            this.urlImage=null;
+        }
+        this.goodAnswer=question.answers[question.correctAnswer];
+    }
+
+    nextQestion(): void {
+        this.numQuestion++;
+        if(this.numQuestion >= this.quiz.questions.length) {
+            console.log("here")
+            this.quiz=null;
+            return;
+        }
+        this.showQuestion();
+    }
+
+    goToTrain(): void {
+        this.router.navigate(['play/'+this.id], {
+            skipLocationChange: false,
+            replaceUrl: true,
+            queryParams: { "trainmode": true }
+        });
     }
 }
